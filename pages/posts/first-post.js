bdbd6f9d9b8d0042df04ada4fb7cd74b8f623449
DevXChardike <chardikeltd@gmail.com>
2023-02-13 15:18:58 +0600

    Create a new component Layout.js and a css module

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,28 +2,29 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import Script from 'next/script';
+import Layout from '../../components/Layout';
 
 export default function FirstPost() {
     return (
         <>
+            <Layout>
+                <Head>
+                    <title>First post title for SEO</title>
+                </Head>
 
-            <Head>
-                <title>First post title for SEO</title>
-            </Head>
-
-            <Script
-                src="https://connect.facebook.net/en_US/sdk.js"
-                strategy="lazyOnload"
-                onLoad={() =>
-                    console.log(`script loaded correctly, window.FB has been populated`)
-                }
-            />
-
-            <h1>First Post</h1>
-            {/* <Image src='/pages/posts/images/profile.jpg'></Image> */}
-            <h2>
-                <Link href="/">Back to home</Link>
-            </h2>
+                <Script
+                    src="https://connect.facebook.net/en_US/sdk.js"
+                    strategy="lazyOnload"
+                    onLoad={() =>
+                        console.log(`script loaded correctly, window.FB has been populated`)
+                    }
+                />
+                <h1>First Post</h1>
+                {/* <Image src='/pages/posts/images/profile.jpg'></Image> */}
+                <h2>
+                    <Link href="/">Back to home</Link>
+                </h2>
+            </Layout>
         </>
     );
 }
